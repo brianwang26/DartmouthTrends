10236c91aa124a4781d4121305f6c994d93788f1
brianwang26 <brian.c.wang.23@dartmouth.edu>
2020-05-17 02:27:40 -0700

    increased guidance for user queries

--- Queries/TestFile.py
+++ Queries/TestFile.py
@@ -1,11 +1,14 @@
-from Queries import TermQueries.py
+from Queries import TermQueries
 
+# List courses you are interested in
+coursesOfInterest = ["WRIT-5", "COSC-10"]
+
+# List terms you are interested in
 # Term Options: TermQueries.allTerms, TermQueries.recentTerms
 termsOfInterest = ["15F", "18F", "19F", "15S", "18W"]
-coursesOfInterest = ["WRIT-5", "COSC-10"]
+
+# Specify whether or not you want to plot grades/enrollments
 plotGrades = True
 plotEnrollments = True
 
 TermQueries.findCoursesTerms(coursesOfInterest, TermQueries.recentTerms, "sample", plotGrades, plotEnrollments)
-
-
