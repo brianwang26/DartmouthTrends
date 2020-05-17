49f717ac8b79d4c5c32f268e6e0a79186ff6f930
brianwang26 <brian.c.wang.23@dartmouth.edu>
2020-05-17 01:33:48 -0700

    Polished User Queries

--- Queries/TermQueries.py
+++ Queries/TermQueries.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+dataTable = pd.read_csv("processedData.csv")
+
 # Create All Terms Option
 termsW = [str(i)+"W" for i in range(11,21)]
 termsS = [str(i)+"S" for i in range(11,20)]
@@ -11,79 +13,79 @@ allTerms = termsW + termsS + termsX + termsF
 # Create Recent Terms Option (since the first term that current seniors participated in)
 recentTerms = ["16F", "17W", "17S", "17X", "17F", "18W", "18S", "18X", "18F", "19W", "19S", "19X", "19F", "20W"]
 
-def findCourses(data, courses, terms = allTerms):
-    frames = []
-    for course in courses:
+# input: list of courses and list of terms; booleans of whether to plot grades and plot enrollments; file name to write result into
+# output: write table of data queried to specified CSV file
+
+def findCoursesTerms(courses, terms, outputFile, plotGrades = False, plotEnrollments = False):
+    frames = [] # initialize resulting frames
+    for course in courses: # split courses by department and number
         tokens = course.split("-")
         courseDept = tokens[0]
         courseNumber = float(tokens[1])
-        frames.append(data[(data['Department'] == courseDept) & (data['Course Number'] == courseNumber) & (data['Term'].isin(terms))])
-    return(pd.concat(frames))
+        frames.append(dataTable[(dataTable['Department'] == courseDept) & (dataTable['Course Number'] == courseNumber) & (dataTable['Term'].isin(terms))])
 
-def plotEnrollment(courseData, courses):
-    currCourse = courseData.iloc[0, 1] + str(courseData.iloc[0,2]) # sets current course to first course
-    currTerm =  courseData.iloc[0, 0] # helps look for terms with multiple sections
-    currEnrollments = [] # holds enrollments of different sections in same term
-    terms = [courseData.iloc[0,0]] # holds all terms (non-repeating)
-    enrollments = [] # holds enrollments per term (combining sections)
+    result = pd.concat(frames)
+    result.to_csv(outputFile + ".csv", index=False)
 
-    for rowNum in range(courseData.shape[0]):
-        if ((courseData.iloc[rowNum, 1] + str(courseData.iloc[rowNum,2])) != currCourse):
-            print(courseData.iloc[rowNum, 1] + str(courseData.iloc[rowNum,2]))
-            print(currCourse)
-            plt.plot(terms, enrollments)
-            currTerm = courseData.iloc[rowNum, 0]
-            currEnrollments = []
-            terms = [courseData.iloc[rowNum, 0]]
-            enrollments = []
+    if(plotGrades):
+        currCourse = result.iloc[0][2] + str(result.iloc[0][3]) # retrieves 1st course from result table
+        currTerms = [result.iloc[0][1]] # instantiates list of terms with first course (terms held in 1st column)
+        currGrades = [result.iloc[0][6]] # will hold the grades of the course (grades held in 6th column)
 
-        if (courseData.iloc[rowNum, 0] != currTerm):
-            enrollments.append(sum(currEnrollments))
-            currEnrollments = []
-            terms.append(courseData.iloc[rowNum, 0])
-            currTerm = courseData.iloc[rowNum, 0]
+        for rowNum in range(result.shape[0]):
+            if ((result.iloc[rowNum][2] + str(result.iloc[rowNum][3]) != currCourse)): # we have arrived at different course
+                # plot previous course
+                plt.plot(currTerms, currGrades)
+                plt.xlabel('Terms')
+                plt.ylabel('Grades by Term')
+                plt.legend([currCourse])
+                plt.show()
 
-        currEnrollments.append(courseData.iloc[rowNum,4])
+                currTerms = [result.iloc[rowNum][1]] # start new terms list
+                currGrades = [result.iloc[rowNum][6]] # start new grades list
+                currCourse = result.iloc[rowNum][2] + str(result.iloc[rowNum][3])
 
-        if(rowNum == courseData.shape[0]-1):
-            enrollments.append(sum(currEnrollments))
+            else:
+                currTerms.append(result.iloc[rowNum][1])
+                currGrades.append(result.iloc[rowNum][6])
 
-    plt.ylabel('Terms')
-    plt.xlabel('Enrollments by Term')
-    plt.legend([course for course in courses], loc='upper left')
-    plt.show()
+        plt.plot(currTerms, currGrades, label = currCourse) # plots for final course in the query
+        plt.xlabel('Terms')
+        plt.ylabel('Grades by Term')
+        plt.legend([currCourse])
+        plt.show()
 
-def plotGrades(courseData):
+    if (plotEnrollments):
+        currCourse = result.iloc[0][2] + str(result.iloc[0][3]) # retrieves 1st course from result table
+        currTerms = [result.iloc[0][1]] # instantiates list of terms with first course (terms held in 1st column)
+        currEnrollments = [result.iloc[0][5]] # will hold the grades of the course (grades held in 6th column)
 
-    currTerm = courseData.iloc[0, 0]  # helps look for terms with multiple sections
-    terms = [courseData.iloc[0, 0]]  # holds all terms (non-repeating)
-    points = []  # holds enrollments per term (combining sections)
-    currPoints = []  # holds enrollments of different sections in same term
+        for rowNum in range(result.shape[0]):
+            if ((result.iloc[rowNum][2] + str(result.iloc[rowNum][3]) != currCourse)): # we have arrived at different course
+                # plot previous course
+                plt.plot(currTerms, currEnrollments)
+                plt.xlabel('Terms')
+                plt.ylabel('Enrollments by Term')
+                plt.legend([currCourse])
+                plt.show()
 
-    for rowNum in range(courseData.shape[0]):
+                currTerms = [result.iloc[rowNum][1]] # start new terms list
+                currEnrollments = [result.iloc[rowNum][5]] # start new grades list
+                currCourse = result.iloc[rowNum][2] + str(result.iloc[rowNum][3])
 
-        if (courseData.iloc[rowNum, 0] != currTerm):
-            points.append(sum(currPoints)/len(currPoints))
-            currPoints = []
-            terms.append(courseData.iloc[rowNum, 0])
-            currTerm = courseData.iloc[rowNum, 0]
+            else:
+                currTerms.append(result.iloc[rowNum][1])
+                currEnrollments.append(result.iloc[rowNum][5])
 
-        currPoints.append(courseData.iloc[rowNum, 6])
-
-        if (rowNum == courseData.shape[0] - 1):
-            points.append(sum(currPoints)/len(currPoints))
-
-    plt.plot(terms, points)
-    plt.ylabel('Terms')
-    plt.xlabel('Median Quality Points')
-    plt.show()
+        # plots for final course in the query
+        plt.plot(currTerms, currEnrollments)
+        plt.xlabel('Terms')
+        plt.ylabel('Enrollments by Term')
+        plt.legend([currCourse])
+        plt.show()
 
 
 termsOfInterest = ["15F", "18F", "19F", "15S", "18W"]
-coursesOfInterest = ["MATH-40", "ENGS-50"]
+coursesOfInterest = ["MATH-20", "ECON-22"]
 
-dataTable = pd.read_csv("data/processedData.csv")
-queryResult = findCourses(dataTable, coursesOfInterest, allTerms)
-print(queryResult)
-# plotEnrollment(queryResult, coursesOfInterest)
-queryResult.to_csv("data/courseTrial.csv", index = False)
+findCoursesTerms(coursesOfInterest, recentTerms, "courseTrial", True, True)
--- Queries/TestFile.py
+++ Queries/TestFile.py
@@ -1,9 +1,9 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from Queries import TermQueries.py
-
-# Term Options: TermQueries.allTerms, TermQueries.recentTerms
-termsOfInterest = []
-coursesOfInterest = []
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from Queries import TermQueries.py
+#
+# # Term Options: TermQueries.allTerms, TermQueries.recentTerms
+# termsOfInterest = []
+# coursesOfInterest = []
 
 
